98f6c269642a5535e69eee27701e3ac2a9864976
sethusaim <sethusaim@gmail.com>
2023-07-23 21:52:54 +0530

    changed prompt

--- main.py
+++ main.py
@@ -3,44 +3,42 @@ from langchain import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.memory import ConversationBufferWindowMemory
+from langchain.memory import ConversationKGMemory
 from langchain.vectorstores import FAISS
 
+embeddings = OpenAIEmbeddings()
+
 template = """
-You have certain patterned questions.Your job is to ask questions related to events only, do not ask any other questions apart from that .
-Your task is to convience the user to look for an event and get thier information which includes name, email and phone number. At the first question you should initiate a conversation asking are you looking for any 
-events and then based on the user response, you should ask the user information one after other and store the user information.
+You are replication of human. Now you are task is to have a friendly conversation and recommend him events nearby based on the conversation
+In the friendly conversation, you might ask the user's hobbies and make sure you engage with him until you understand the user's behaviour. 
+By analyzing his behaviour try to bring the user to our objective of going to events by your friendly conversation. Use the conversation style from the data source provided.
+Do not ask repeated questions. After 3 questions try to recommend some nearby events based on the user's information previously gathered.
+The AI ONLY uses information contained in the "Relevant Information" section and does not hallucinate.
 {context}
-
-Question: {question}
-Answer:
+Question : {question}
+Answer:  
 """
-
 
 @cl.on_chat_start
 async def main():
-    prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+    prompt = PromptTemplate(template=template, input_variables=["question", "context"])
+    
+    llm =  ChatOpenAI(temperature=0.2)
 
     vectorstore = FAISS.load_local(
         folder_path="embeddings", index_name="qa_index", embeddings=embeddings,
     )
-    
-    embeddings = OpenAIEmbeddings()
 
-    memory = ConversationBufferWindowMemory(
-        k=7, return_messages=True, memory_key="chat_history"
-    )
+    memory = ConversationKGMemory(llm=llm)
 
     qa_chain = RetrievalQA.from_llm(
-        llm=ChatOpenAI(temperature=0.2),
+        llm=llm,
         prompt=prompt,
         memory=memory,
         retriever=vectorstore.as_retriever(),
     )
 
-    await cl.AskUserMessage(
-        content="Hello, how are you?. Are you looking for any event ?",timeout=10
-    ).send()
+    await cl.AskUserMessage(content="Hello, how are you?", timeout=10).send()
 
     cl.user_session.set("llm_chain", qa_chain)
 
